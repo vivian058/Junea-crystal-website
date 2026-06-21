24addf4d30f41c40973d01d95b052ec0a399afab
vivian058 <vivian19930324@gmail.com>
2026-06-21 22:29:25 +0800

    Add files via upload

--- pages/crystal-cost.js
+++ pages/crystal-cost.js
@@ -269,7 +269,7 @@ function renderTable(records) {
     </tr>`;
 
     extra.forEach(r => {
-      rows += `<tr class="${gid}-hist" style="display:none;background:#f5f4f4">
+      rows += `<tr class="${gid}-hist" style="display:none;background:#f6f6f6">
         <td style="text-align:center;padding:8px 6px">
           <input type="checkbox" class="row-check" value="${r.id}" onchange="updateBulkBar()">
         </td>
